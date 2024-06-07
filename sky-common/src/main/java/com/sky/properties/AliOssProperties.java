6a1364b7010e3d140b6d3ea006348e408679d266
Jaspeeerzhan <zz4026@nyu.edu>
2024-06-07 05:46:33 -0400

    SkyDelivery Category and Dish and Setmeal Sections  and AutoFill AOP Completed

--- sky-common/src/main/java/com/sky/properties/AliOssProperties.java
+++ sky-common/src/main/java/com/sky/properties/AliOssProperties.java
@@ -1,5 +1,7 @@
 package com.sky.properties;
 
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -8,10 +10,6 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "sky.alioss")
 @Data
 public class AliOssProperties {
-
     private String endpoint;
-    private String accessKeyId;
-    private String accessKeySecret;
     private String bucketName;
-
 }
